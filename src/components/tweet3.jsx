85a209d0b56d8f100590dbb01068cc3872c63d5b
Nworie Kingsley <obizzkingsley@gmail.com>
2020-10-26 17:29:56 +0100

    demotivated

--- src/components/tweet3.jsx
+++ src/components/tweet3.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-const TweetContainer = ({msg}) => {
+const TweetContainer = ({ msg }) => {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://platform.twitter.com/widgets.js";
@@ -9,9 +9,22 @@ const TweetContainer = ({msg}) => {
 
   return (
     <section className="twitterContainer  col-6 tweet-well">
-     
-     <blockquote className="twitter-tweet"><p lang="en" dir="ltr">Wow just finished checking out your content on YouTube and I would recommend it for anyone that wants to learn Graphics Design <a href="https://twitter.com/nworiekingslee?ref_src=twsrc%5Etfw">@nworiekingslee</a> <a href="https://twitter.com/hashtag/developers?src=hash&amp;ref_src=twsrc%5Etfw">#developers</a></p>&mdash; Winter❄ #ENDBADGOVERANCEINNIGERIA ⚪ (@ezekwem_chris) <a href="https://twitter.com/ezekwem_chris/status/1318222486153068546?ref_src=twsrc%5Etfw">October 19, 2020</a></blockquote>
-      
+      <blockquote className="twitter-tweet">
+        <p lang="en" dir="ltr">
+          Wow just finished checking out your content on YouTube and I would
+          recommend it for anyone that wants to learn Graphics Design{" "}
+          <a href="https://twitter.com/nworiekingslee?ref_src=twsrc%5Etfw">
+            @nworiekingslee
+          </a>{" "}
+          <a href="https://twitter.com/hashtag/developers?src=hash&amp;ref_src=twsrc%5Etfw">
+            #developers
+          </a>
+        </p>
+        &mdash; Winter❄ #ENDBADGOVERANCEINNIGERIA <span role="img" arial-label="emoji">⚪</span> (@ezekwem_chris){" "}
+        <a href="https://twitter.com/ezekwem_chris/status/1318222486153068546?ref_src=twsrc%5Etfw">
+          October 19, 2020
+        </a>
+      </blockquote>
     </section>
   );
 };
